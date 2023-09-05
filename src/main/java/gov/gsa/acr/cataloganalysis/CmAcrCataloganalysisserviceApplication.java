6f427cf1c04073cfa7546bdac8eead2e37b12a4c
AnupamChandra <anupam.chandra@gsa.gov>
2023-09-05 11:13:37 -0400

    Very first commit!

---
+++ src/main/java/gov/gsa/acr/cataloganalysis/CmAcrCataloganalysisserviceApplication.java
@@ -0,0 +1,13 @@
+package gov.gsa.acr.cataloganalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmAcrCataloganalysisserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmAcrCataloganalysisserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gov/gsa/acr/cataloganalysis/CmAcrCataloganalysisserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.gsa.acr.cataloganalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmAcrCataloganalysisserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
