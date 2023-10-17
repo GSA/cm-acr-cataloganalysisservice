791a3b7f7e218917708d4d6d9e74ed8c53d8ca66
AnupamChandra <anupam.chandra@gsa.gov>
2023-10-17 15:25:49 -0400

    Add Support for S3.

--- src/main/java/gov/gsa/acr/cataloganalysis/service/XsbDataService.java
+++ src/main/java/gov/gsa/acr/cataloganalysis/service/XsbDataService.java
@@ -61,7 +61,7 @@ public class XsbDataService {
 
 
     @Transactional
-    private Mono<Void> moveXsbData(Integer numRecordsInStaging, Sinks.Many<String> statusNotifier) {
+    protected Mono<Void> moveXsbData(Integer numRecordsInStaging, Sinks.Many<String> statusNotifier) {
         return xsbDataRepository.deleteAll()
                 .doFirst(() -> {
                     if (statusNotifier != null) {
