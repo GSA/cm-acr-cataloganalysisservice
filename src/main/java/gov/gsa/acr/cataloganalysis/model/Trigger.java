354d6fa8dd14e7818456f60e809dddee9be58e1a
AnupamChandra <anupam.chandra@gsa.gov>
2023-10-18 13:51:12 -0400

    Added Swagger Documentation.

--- src/main/java/gov/gsa/acr/cataloganalysis/model/Trigger.java
+++ src/main/java/gov/gsa/acr/cataloganalysis/model/Trigger.java
@@ -1,6 +1,7 @@
 package gov.gsa.acr.cataloganalysis.model;
 
 import gov.gsa.acr.cataloganalysis.util.XsbSourceType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.util.HashSet;
@@ -8,9 +9,32 @@ import java.util.Set;
 
 @Data
 public class Trigger {
+    @Schema(description = """
+    Source for XSB Files. Has to be one of "SFTP", or "LOCAL", or "S3". 
+    "SFTP": will look for files on the XSB's SFTP server, 
+    "S3": Will look for file in the ACR's S3 bucket, 
+    "LOCAL": Will look for files on the PODs local file system.
+    """)
     private XsbSourceType sourceType;
+    @Schema(description = """
+            The folder relative to the base of the source. For example, for the SFTP source the base is the root folder "/".
+            For the S3 bucket the base is the folder "catalogAnalysis"  and for the local file system the base is the 
+            root folder "/".
+            """,
+            requiredMode = Schema.RequiredMode.NOT_REQUIRED
+    )
     private String sourceFolder;
+    @Schema(description = """
+            A list of either full file names or glob like patterns. e.g.  
+            "files" : [ "47QSMA21D08R6-7000039_20230901135843_5367723946113572875_report_1.gsa", "47QSWA18D000C-3008711_20230907134812_7055515986367968069_report_1.gsa" ]
+            or 
+            "files" : ['myTestSftp*.gsa', "47QSWA19D0073-3003521**"]
+            
+            Except in case where the source is S3 bucket. The glob pattern does not work in case of S3, the the values in the files array
+            are used as prefix only for looking for files in the S3 bucket.
+            """)
     private String[] files;
+    @Schema(hidden = true)
     private Set<String> uniqueFileNames;
 
     public Set<String> getUniqueFileNames(){
--- src/main/java/gov/gsa/acr/cataloganalysis/restservices/XsbDataController.java
+++ src/main/java/gov/gsa/acr/cataloganalysis/restservices/XsbDataController.java
@@ -2,6 +2,10 @@ package gov.gsa.acr.cataloganalysis.restservices;
 
 import gov.gsa.acr.cataloganalysis.model.Trigger;
 import gov.gsa.acr.cataloganalysis.service.XsbDataService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -22,8 +26,70 @@ public class XsbDataController extends BaseController{
         this.xsbDataService = xsbDataService;
     }
 
+    @Operation(summary = "Trigger (start) the XSB data upload process.",
+            description = """
+    This API endpoint is used to trigger the Catalog Analysis Service to start loading the bi-monthly XSB data. The end point expects
+    a trigger message. The examples listed below explains the structure of the trigger message and explains what each attribute means.
+    """
+    )
     @PostMapping(value="/trigger", produces = MediaType.APPLICATION_NDJSON_VALUE)
-    public Mono<String> trigger(@RequestBody Trigger trigger){
+    public Mono<String> trigger(@io.swagger.v3.oas.annotations.parameters.RequestBody(content = {
+            @Content(
+            schema = @Schema(implementation = Trigger.class),
+            examples = {
+                    @ExampleObject(
+                            name = "Sample Trigger Object from SFTP server",
+                            summary = "SFTP Example",
+                            description = """
+                            To trigger downloading files from XSB's SFTP server. The sourceFolder attribute  is not required as the
+                            default value is configured already in the system. The "files" property could be an array of full file names
+                            or glob like patterns.
+                            """,
+                            value = """
+                                    {"sourceType": "SFTP",
+                                     "files": ["47QSMA21D08R6-7000039_20230919195858_4325137760202194341_report_1.gsa", "47QSWA19D0073-3003521*"]                     
+                                    }
+                                    """
+                                    ),
+                    @ExampleObject(
+                            name = "Sample Trigger Object from S3 bucket",
+                            summary = "S3 Example",
+                            description = """
+                            To trigger downloading files from ACR's S3 bucket. 
+                            If the sourceFolder is provided, it is relative to "catalogAnalysis" on S3, meaning the base is always "catalogAnalysis", so in the example, the files will be looked relative to catalogAnalysis/testData/. 
+                            If the sourceFolder is not provided, the files will be looked under catalogAnalysis.
+                            The "files" property could be an array of full file names
+                            or prefix, since glob patterns do not work in case of S3.
+                            """,
+                            value = """
+                                    {"sourceType": "S3",
+                                    "sourceFolder":"testData",
+                                     "files": ["47QSMA21D08R6-7000039_20230919195858_4325137760202194341_report_1.gsa", "47QSWA19D0073-3003521"]                     
+                                    }
+                                    """
+                    )
+                    ,
+                    @ExampleObject(
+                            name = "Sample Trigger Object from the local file system",
+                            summary = "Local Example",
+                            description = """
+                            To trigger downloading files from the pod's local file system. 
+                            SourceFolder is required in this case and it should be the absolute path to the files.
+                            The "files" property could be an array of full file names
+                            or glob like patterns.
+                            """,
+                            value = """
+                                    {"sourceType": "LOCAL",
+                                    "sourceFolder":"/app/testData",
+                                     "files": ["47QSMA21D08R6-7000039_20230919195858_4325137760202194341_report_1.gsa", "47QSWA19D0073-3003521"]                     
+                                    }
+                                    """
+                    )
+
+
+            })})
+
+                                    @RequestBody Trigger trigger){
         log.info("Request body " + trigger);
         String message;
         try {
