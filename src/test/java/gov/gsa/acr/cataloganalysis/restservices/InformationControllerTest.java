78a0ad7a5a473a6087b57d0a0d63dd4c56931b48
AnupamChandra <anupam.chandra@gsa.gov>
2023-11-15 15:55:07 -0500

    Changed the Rest Controller tests to actual WebFlux tests using WebTestClient.

--- src/test/java/gov/gsa/acr/cataloganalysis/restservices/InformationControllerTest.java
+++ src/test/java/gov/gsa/acr/cataloganalysis/restservices/InformationControllerTest.java
@@ -3,28 +3,39 @@ package gov.gsa.acr.cataloganalysis.restservices;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@SpringBootTest
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Slf4j
-@ContextConfiguration(classes ={InformationController.class})
 @TestPropertySource(locations="classpath:application-test.properties")
+@AutoConfigureWebTestClient
 class InformationControllerTest {
-
     @Autowired
-    InformationController informationController;
+    WebTestClient webTestClient;
 
     @Test
-    void greet() {
-        assertEquals("Welcome to Catalog Analysis",informationController.greet());
+    void testRootEndPoint() {
+        webTestClient
+                // Create a GET request to test an endpoint
+                .get().uri("/api")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class).value(greeting -> assertThat(greeting).isEqualTo("Welcome to Catalog Analysis"));
     }
 
     @Test
-    void info() {
-        assertEquals("A service for analyzing catalogs in ACR", informationController.info());
+    void testInfoEndPoint() {
+        webTestClient
+                // Create a GET request to test an endpoint
+                .get().uri("/api/info")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class).value(greeting -> assertThat(greeting).isEqualTo("A service for analyzing catalogs in ACR"));
     }
 }
