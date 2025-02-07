b124a415ca992dc0dc282dcf5dc8d5f82f16a2ba
Justin Babbs <justin.babbs@gsa.gov>
2025-02-07 10:43:14 -0500

    Revert "Merge pull request #121 from ACR/feature/ACREPO-4798"
    This reverts commit c01903c839fafea476fb11e216fae1a786926e0f, reversing
    changes made to 32c01e2705db91066626e19a4b28160bb28c1cf9.

--- src/main/java/gov/gsa/acr/cataloganalysis/model/XsbDataJsonRecord.java
+++ src/main/java/gov/gsa/acr/cataloganalysis/model/XsbDataJsonRecord.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 @JsonPropertyOrder({
         "catalogMedianPrice",
         "catalogMedianPriceSupplier",
