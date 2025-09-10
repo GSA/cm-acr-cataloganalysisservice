8ab9a4944f855b40dae84fd421983396bb28562c
AnupamChandra <anupam.chandra@gsa.gov>
2025-09-10 11:23:31 -0400

    ACREPO-5593:  Experimenting with scheduling.

--- src/main/java/gov/gsa/acr/cataloganalysis/restservices/BaseController.java
+++ src/main/java/gov/gsa/acr/cataloganalysis/restservices/BaseController.java
@@ -1,6 +1,8 @@
 package gov.gsa.acr.cataloganalysis.restservices;
 
+import lombok.Generated;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/api")
+@Generated
 public class BaseController {}
